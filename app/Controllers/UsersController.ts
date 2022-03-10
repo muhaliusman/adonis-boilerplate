18828f370f321c95d4de76426354a1ec04df73aa
Muhammad Ali Usman <aliusmanwork07@gmail.com>
2022-03-10 20:07:32 +0700

    Crud user

--- app/Controllers/UsersController.ts
+++ app/Controllers/UsersController.ts
@@ -1,4 +1,6 @@
 import User from 'App/Models/User'
+import StoreUser from 'App/Validators/StoreUserValidator'
+import UpdateUser from 'App/Validators/UpdateUserValidator'
 
 export default class UsersController {
   public async index({ request, response }) {
@@ -17,5 +19,52 @@ export default class UsersController {
     users.baseUrl('/users')
 
     return response.send(users)
+  }
+
+  public async show({ params, response }) {
+    const id = params.id
+
+    const user = await User.findOrFail(id)
+
+    return response.send(user)
+  }
+
+  public async store({ request, response }) {
+    const payload = await request.validate(StoreUser)
+
+    const user = new User()
+    user.email = payload.email
+    user.name = payload.name
+    user.password = payload.password
+    await user.save()
+
+    return response.status(201).send(user)
+  }
+
+  public async update({ request, response, params }) {
+    const payload = await request.validate(UpdateUser)
+    const id = params.id
+
+    const user = await User.findOrFail(id)
+    user.email = payload.email
+    user.name = payload.name
+
+    if (payload.password) {
+      user.password = payload.password
+    }
+
+    await user.save()
+
+    return response.send(user)
+  }
+
+  public async destroy({ response, params }) {
+    const id = params.id
+
+    const user = await User.findOrFail(id)
+
+    await user.delete()
+
+    return response.send(user)
   }
 }
